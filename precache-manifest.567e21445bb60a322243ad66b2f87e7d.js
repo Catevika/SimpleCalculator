f892c98b676e1ffec58110a014319e055359fcc3
Catevika <dominique.bello@outlook.fr>
2020-03-17 20:28:10 +0100

    Updates

---
+++ precache-manifest.567e21445bb60a322243ad66b2f87e7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5e6670becd33d0b0ee7c7d5fde67d5",
+    "url": "/SimpleCalculator/index.html"
+  },
+  {
+    "revision": "588efa2e144aeeaf9637",
+    "url": "/SimpleCalculator/static/css/main.04151271.chunk.css"
+  },
+  {
+    "revision": "82324bf8d5f0046b3071",
+    "url": "/SimpleCalculator/static/js/2.7b815369.chunk.js"
+  },
+  {
+    "revision": "98cef8f2c6f4a9493000de24ffc1680a",
+    "url": "/SimpleCalculator/static/js/2.7b815369.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "588efa2e144aeeaf9637",
+    "url": "/SimpleCalculator/static/js/main.86492f33.chunk.js"
+  },
+  {
+    "revision": "43319df1e65da8e07974",
+    "url": "/SimpleCalculator/static/js/runtime-main.81bbf6bf.js"
+  }
+]);
